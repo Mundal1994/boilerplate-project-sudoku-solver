df50bd0c7481e52a80788b248d04a5fb86c5cc56
Mundal1994 <manga_flip@hotmail.com>
2024-07-25 00:16:39 +0300

    prework of adding tests to file

--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -5,5 +5,40 @@ const Solver = require('../controllers/sudoku-solver.js');
 let solver;
 
 suite('Unit Tests', () => {
-
+    test('valid puzzle string of 81 characters', function(){
+        //assert.equal(, );
+    });
+    test('puzzle string with invalid characters (not 1-9 or .)', function(){
+        //assert.equal(, );
+    });
+    test('puzzle string that is not 81 characters in length', function(){
+        //assert.equal(, );
+    });
+    test('valid row placement', function(){
+        //assert.equal(, );
+    });
+    test('invalid row placement', function(){
+        //assert.equal(, );
+    });
+    test('valid column placement', function(){
+        //assert.equal(, );
+    });
+    test('invalid column placement', function(){
+        //assert.equal(, );
+    });
+    test('valid region (3x3 grid) placement', function(){
+        //assert.equal(, );
+    });
+    test('invalid region (3x3 grid) placement', function(){
+        //assert.equal(, );
+    });
+    test('Valid puzzle strings pass the solver', function(){
+        //assert.equal(, );
+    });
+    test('Invalid puzzle strings fail the solver', function(){
+        //assert.equal(, );
+    });
+    test('Solver returns the expected solution for an incomplete puzzle', function(){
+        //assert.equal(, );
+    });
 });
--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -6,6 +6,50 @@ const server = require('../server');
 chai.use(chaiHttp);
 
 suite('Functional Tests', () => {
-
+    /*suite('Solve a Puzzle Tests', () => {
+        test('valid puzzle string: POST request to /api/solve', function(done){
+            //done
+        });
+        test('missing puzzle string: POST request to /api/solve', function(done){
+            //done
+        });
+        test('invalid characters: POST request to /api/solve', function(done){
+            //done
+        });
+        test('incorrect length: POST request to /api/solve', function(done){
+            //done
+        });
+        test('Solve a puzzle that cannot be solved: POST request to /api/solve', function(done){
+            //done
+        });
+    });*/
+    /*suite('Check a Puzzle Tests', () => {
+        test('a puzzle placement with all fields: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with single placement conflict: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with multiple placement conflicts: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with all placement conflicts: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with missing required fields: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with invalid characters: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with incorrect length: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with invalid placement coordinate: POST request to /api/check', function(done){
+            //done
+        });
+        test('a puzzle placement with invalid placement value: POST request to /api/check', function(done){
+            //done
+        });
+    });*/
 });
-

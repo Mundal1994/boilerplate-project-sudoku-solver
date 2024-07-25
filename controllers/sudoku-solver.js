db31fc9fa60462bc1c845bf8db562039d811211a
Mundal1994 <manga_flip@hotmail.com>
2024-07-25 16:40:31 +0300

    fixed error in Region placement, and implemented validated function + test

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,32 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    const len = puzzleString.length;
+
+    if (len != 81) {
+      console.log("Error: puzzle string is not 81 characters long, current len: ", len);
+      return false;
+    }
+    const validChar = '123456789.';
+
+    for (let i = 0; i < len; i++) {
+      if (!validChar.includes(puzzleString[i])) {
+        console.log("Error: puzzle string contains unvalid character: ", puzzleString[i]);
+        return false;
+      }
+      if (puzzleString[i] != '.') {
+        const row = Math.floor((i / 9) + 1);
+        const col = (i % 9) + 1;
+        const val = puzzleString[i];
+        
+        if (!this.checkRowPlacement(puzzleString, row, col, val) || 
+          !this.checkColPlacement(puzzleString, row, col, val) || 
+          !this.checkRegionPlacement(puzzleString, row, col, val)) {
+          return false;
+        }
+      }
+    }
+    return true;
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -11,12 +37,12 @@ class SudokuSolver {
 
     if (valPos < len && valPos >= 0) {
       while (start < end) {
-        if (puzzleString[start] == value) {
+        if (start != valPos && puzzleString[start] == value) {
+          console.log("Row Placement is false: Pos: ", start, " with value: ", value, " already exist");
           return false;
         }
         ++start;
       }
-
       return true;
     }
     return false;
@@ -30,7 +56,8 @@ class SudokuSolver {
   
     if (valPos < len && valPos >= 0) {
       while (start < end) {
-        if (puzzleString[start] == value) {
+        if (start != valPos && puzzleString[start] == value) {
+          console.log("Column placement is false: Pos: ", start, " with value: ", value, " already exist");
           return false;
         }
         start += 9;
@@ -43,28 +70,29 @@ class SudokuSolver {
 
   checkRegionPlacement(puzzleString, row, column, value) {
     const len = puzzleString.length;
-    let startCol = 5;
+    let startCol = 6;
     if (column < 4) {
       startCol = 0;
     } else if (column < 7) {
       startCol = 3;
     }
 
-    let startRow = 5;
+    let startRow = 6;
     if (row < 4) {
       startRow = 0;
-    } else if (column < 7) {
+    } else if (row < 7) {
       startRow = 3;
     }
   
     let start = (9 * (startRow)) + startCol;
     const end = (9 * (startRow + 2)) + (startCol + 3);
     const valPos = (9 * (row - 1)) + column - 1;
-  
+
     if (valPos < len && valPos >= 0) {
       while (start < end) {
         for (let i = 0; i < 3; i++) {
-          if (puzzleString[start + i] == value) {
+          if (start + i != valPos && puzzleString[start + i] == value) {
+            console.log("Region placement is false: Pos: ", start + i, " with value: ", value, " already exist");
             return false;
           }
         }
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -7,13 +7,25 @@ let solver = new Solver;
 
 suite('Unit Tests', () => {
     test('valid puzzle string of 81 characters', function(){
-        //assert.equal(, );
+        const len = Puzzle.puzzlesAndSolutions.length;
+        
+        for (let i = 0; i < len; i++) {
+            assert.isTrue(solver.validate(Puzzle.puzzlesAndSolutions[i][0]));
+        }
     });
     test('puzzle string with invalid characters (not 1-9 or .)', function(){
-        //assert.equal(, );
+        assert.isFalse(solver.validate('1.5..2.84..63.12.7.2..5.....90.1....8.2.3674.3.7.2..9.47...8..1..16....926914.37.'));
+        assert.isFalse(solver.validate('5..91372.3...8.5.9.9.25..8_68.47.23...95..46.7.4.....5.2.......4..8916..85.72...3'));
+        assert.isFalse(solver.validate('..839.7.575.....964.M1.......16.29846.9.312.7..754.....62..5.78.8...3.2...492...1'));
+        assert.isFalse(solver.validate('.7.89.....5....3.4.2..4..l.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6'));
+        assert.isFalse(solver.validate('82..4..6...16..89...98315.749.157.............S3..4...96.415..81..7632..3...28.51'));
     });
     test('puzzle string that is not 81 characters in length', function(){
-        //assert.equal(, );
+        assert.isFalse(solver.validate('1.5..2.84..63.12.7.2..5.....9..1....8.2.3674.3.7.2..9.47...8..1..16....926914.37'));
+        assert.isFalse(solver.validate('5..91372.3...8.5.9.9.25..8.68.47.23...'));
+        assert.isFalse(solver.validate(''));
+        assert.isFalse(solver.validate('.7.89.....5....3.4.2..4..1.5689..472...6.....1.7.5.63873.1.2.8.6..47.1..2.9.387.6.'));
+        assert.isFalse(solver.validate('82..4..6...16..89...98315.749.157.............53..4...96.415..81..7632..3...28.51..............'));
     });
     test('valid row placement', function(){
         assert.isTrue(solver.checkRowPlacement(Puzzle.puzzlesAndSolutions[0][0], 1, 2, 3));
@@ -34,7 +46,7 @@ suite('Unit Tests', () => {
         assert.isTrue(solver.checkColPlacement(Puzzle.puzzlesAndSolutions[1][0], 1, 2, 4));
     });
     test('invalid column placement', function(){
-        assert.isFalse(solver.checkColPlacement(Puzzle.puzzlesAndSolutions[0][0], 1, 9, 4));
+        assert.isFalse(solver.checkColPlacement(Puzzle.puzzlesAndSolutions[0][0], 3, 9, 4));
         assert.isFalse(solver.checkColPlacement(Puzzle.puzzlesAndSolutions[2][0], 8, 9, 1));
         assert.isFalse(solver.checkColPlacement(Puzzle.puzzlesAndSolutions[0][0], 2, 8, 8));
         assert.isFalse(solver.checkColPlacement(Puzzle.puzzlesAndSolutions[1][0], 2, 2, 9));
@@ -48,7 +60,7 @@ suite('Unit Tests', () => {
     test('invalid region (3x3 grid) placement', function(){
         assert.isFalse(solver.checkRegionPlacement(Puzzle.puzzlesAndSolutions[0][0], 1, 2, 5));
         assert.isFalse(solver.checkRegionPlacement(Puzzle.puzzlesAndSolutions[2][0], 9, 8, 2));
-        assert.isFalse(solver.checkRegionPlacement(Puzzle.puzzlesAndSolutions[0][0], 9, 6, 2));
+        assert.isFalse(solver.checkRegionPlacement(Puzzle.puzzlesAndSolutions[0][0], 9, 6, 8));
         assert.isFalse(solver.checkRegionPlacement(Puzzle.puzzlesAndSolutions[1][0], 1, 2, 3));
     });
     test('Valid puzzle strings pass the solver', function(){
